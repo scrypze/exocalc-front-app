fc7e4609a0fc93372404ec8c7e94f679d9eab45a
eordiyants <e.ordiyants@ctrl2go.solutions>
2025-11-17 02:08:47 +0300

    добавил команду и настроил роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/exocalc-front-app">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
